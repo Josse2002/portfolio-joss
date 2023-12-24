df7654476b2594bccdcdddf4855b0ddd8c1c3f92
Josse2002 <00048322@uca.edu.sv>
2023-12-24 01:51:35 -0600

    repairing react vite

--- portfolio/vite.config.js
+++ portfolio/vite.config.js
@@ -1,7 +1,10 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-export default {
-  // ...otras configuraciones...
-  plugins: [
-   
-  ],
-};
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: "0.0.0.0",
+  },
+});
