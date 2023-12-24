a9d20a668a8c283c7ebeb38330f494a9f7c9cc1c
Josse2002 <00048322@uca.edu.sv>
2023-12-24 01:53:39 -0600

    quit hello in app

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -14,7 +14,6 @@ function App() {
       <div className="w-full lg:w-1/2">
       <I18nextProvider i18n={i18n}>
         <Header />
-        <h1>Holaa</h1>
         <Resume />
         <AboutMe />
         <Timeline />
